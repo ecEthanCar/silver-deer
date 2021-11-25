c9a0e4105e75fbbde735ac7fc7bc197e1adc669c
Qiyu Hu <qh95@cornell.edu>
2021-11-25 18:09:03 -0500

    homepage layout, add hover interactivity

---
+++ scripts/expectations.js
@@ -0,0 +1,61 @@
+console.log("start")
+
+
+$("#equity").hover(function() {
+  // TODO: snippet(s) to respond to when the mouse enters the element.
+  console.log("hi")
+  $("#equity-description").removeClass("hidden")
+}, function() {
+  // TODO: snippet(s) to respond to when the mouse leaves the element.
+  // $("#equity-description").addClass("hidden")
+});
+
+
+$("#solution-oriented").hover(function() {
+  // TODO: snippet(s) to respond to when the mouse enters the element.
+  console.log("hi")
+  $("#solution-description").removeClass("hidden")
+}, function() {
+  // TODO: snippet(s) to respond to when the mouse leaves the element.
+  $("#solution-description").addClass("hidden")
+});
+
+
+$("#research-based").hover(function() {
+  // TODO: snippet(s) to respond to when the mouse enters the element.
+  console.log("hi")
+  $("#research-description").removeClass("hidden")
+}, function() {
+  // TODO: snippet(s) to respond to when the mouse leaves the element.
+  $("#research-description").addClass("hidden")
+});
+
+
+$("#social-learning").hover(function() {
+  // TODO: snippet(s) to respond to when the mouse enters the element.
+  console.log("hi")
+  $("#social-description").removeClass("hidden")
+}, function() {
+  // TODO: snippet(s) to respond to when the mouse leaves the element.
+  $("#social-description").addClass("hidden")
+});
+
+
+$("#network").hover(function() {
+  // TODO: snippet(s) to respond to when the mouse enters the element.
+  console.log("hi")
+  $("#network-description").removeClass("hidden")
+}, function() {
+  // TODO: snippet(s) to respond to when the mouse leaves the element.
+  $("#network-description").addClass("hidden")
+});
+
+
+$("#instructors").hover(function() {
+  // TODO: snippet(s) to respond to when the mouse enters the element.
+  console.log("hi")
+  $("#instructors-description").removeClass("hidden")
+}, function() {
+  // TODO: snippet(s) to respond to when the mouse leaves the element.
+  $("#instructors-description").addClass("hidden")
+});
