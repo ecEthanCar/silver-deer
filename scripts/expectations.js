3216626f70088abc6e8e9b29a7b14af83c498dd1
Qiyu Hu <qh95@cornell.edu>
2021-11-25 20:18:59 -0500

    finished hover interactivity

--- scripts/expectations.js
+++ scripts/expectations.js
@@ -2,60 +2,60 @@ console.log("start")
 
 
 $("#equity").hover(function() {
-  // TODO: snippet(s) to respond to when the mouse enters the element.
+  // remove class hidden from #equity-description when the mouse leaves the element.
   console.log("hi")
   $("#equity-description").removeClass("hidden")
 }, function() {
-  // TODO: snippet(s) to respond to when the mouse leaves the element.
-  // $("#equity-description").addClass("hidden")
+  // add class hidden to #equity-description when the mouse leaves the element.
+  $("#equity-description").addClass("hidden")
 });
 
 
 $("#solution-oriented").hover(function() {
-  // TODO: snippet(s) to respond to when the mouse enters the element.
+  // remove class hidden when the mouse leaves the element.
   console.log("hi")
   $("#solution-description").removeClass("hidden")
 }, function() {
-  // TODO: snippet(s) to respond to when the mouse leaves the element.
+  // add class hidden when the mouse leaves the element.
   $("#solution-description").addClass("hidden")
 });
 
 
 $("#research-based").hover(function() {
-  // TODO: snippet(s) to respond to when the mouse enters the element.
+  // remove class hidden when the mouse leaves the element.
   console.log("hi")
   $("#research-description").removeClass("hidden")
 }, function() {
-  // TODO: snippet(s) to respond to when the mouse leaves the element.
+  // add class hidden when the mouse leaves the element.
   $("#research-description").addClass("hidden")
 });
 
 
 $("#social-learning").hover(function() {
-  // TODO: snippet(s) to respond to when the mouse enters the element.
+  // remove class hidden when the mouse leaves the element.
   console.log("hi")
   $("#social-description").removeClass("hidden")
 }, function() {
-  // TODO: snippet(s) to respond to when the mouse leaves the element.
+  // add class hidden when the mouse leaves the element.
   $("#social-description").addClass("hidden")
 });
 
 
 $("#network").hover(function() {
-  // TODO: snippet(s) to respond to when the mouse enters the element.
+  // remove class hidden when the mouse leaves the element.
   console.log("hi")
   $("#network-description").removeClass("hidden")
 }, function() {
-  // TODO: snippet(s) to respond to when the mouse leaves the element.
+  // add class hidden when the mouse leaves the element.
   $("#network-description").addClass("hidden")
 });
 
 
 $("#instructors").hover(function() {
-  // TODO: snippet(s) to respond to when the mouse enters the element.
+  // remove class hidden when the mouse leaves the element.
   console.log("hi")
   $("#instructors-description").removeClass("hidden")
 }, function() {
-  // TODO: snippet(s) to respond to when the mouse leaves the element.
+  // add class hidden when the mouse leaves the element.
   $("#instructors-description").addClass("hidden")
 });
